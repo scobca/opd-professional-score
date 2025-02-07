ad7073f38825cad0f06e6a55dc703006750f1b01
Vladimir <scobca18@yandex.ru>
2025-02-08 01:18:05 +0300

    feat(secure): created bcrypt.util.ts

---
+++ src/utils/bcrypt.util.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptUtil {
+  public async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(password, salt);
+  }
+
+  public async compare(password: string, hash: string) {
+    return await bcrypt.compare(password, hash);
+  }
+}
