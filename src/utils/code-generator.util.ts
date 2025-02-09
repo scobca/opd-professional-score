41de51654948412da17515f9c9b9a0a5ec71e0ea
Vladimir <scobca18@yandex.ru>
2025-02-09 05:12:10 +0300

    feat(auth): created random code generator

---
+++ src/utils/code-generator.util.ts
@@ -0,0 +1,9 @@
+export class CodeGeneratorUtil {
+  public generateCode(): string {
+    const min = 100000;
+    const max = 999999;
+    const code = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return code.toString();
+  }
+}
