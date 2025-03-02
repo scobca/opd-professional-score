81b1f4232381e94204d8f4debf56867ff1a44eed
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-02 18:41:09 +0300

    fix(migrations): update table fields

--- src/config/migrations/profession_scores.sql
+++ src/config/migrations/profession_scores.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS profession_scores
 (
-    id           SERIAL PRIMARY KEY,
-    professionId INTEGER,
-    profCharId   INTEGER,
-    userId       INTEGER,
-    score        INTEGER,
-    createdAt    timestamptz,
-    updatedAt    timestamptz,
-    CONSTRAINT fk_profession FOREIGN KEY (professionId) REFERENCES professions (id),
-    CONSTRAINT fk_profChar FOREIGN KEY (profCharId) REFERENCES professional_characteristics (id),
-    CONSTRAINT fk_user FOREIGN KEY (userId) REFERENCES "user" (id)
+    "id"           SERIAL PRIMARY KEY,
+    "professionId" INTEGER,
+    "profCharId"   INTEGER,
+    "userId"       INTEGER,
+    "score"        INTEGER,
+    "createdAt"    timestamptz,
+    "updatedAt"    timestamptz,
+    CONSTRAINT fk_profession FOREIGN KEY ("professionId") REFERENCES professions (id),
+    CONSTRAINT fk_profChar FOREIGN KEY ("profCharId") REFERENCES professional_characteristics (id),
+    CONSTRAINT fk_user FOREIGN KEY ("userId") REFERENCES "user" (id)
 )
