85f55cf18837151222f20ce37b6da4998244368b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-03-20 00:13:49 +0300

    feat(mailing): added ability to send verification code again

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,10 +5,16 @@ import { BasicSuccessfulResponse } from '../IO/basic-successful-response';
 import { SuccessAuthResponseDto } from '../dto/auth/success-auth-response.dto';
 import { RegistrationFirstStepDto } from '../dto/auth/registration-first-step.dto';
 import { RegistrationSecondStepDto } from '../dto/auth/registration-second-step.dto';
+import { SendCodeAgainDto } from '../dto/auth/send-code-again.dto';
 
 @Controller('/auth')
 export class AuthController {
   constructor(@Inject(AuthProvider) private authProvider: AuthProvider) {}
+
+  @Post('/sendCodeAgain')
+  public async sendCodeAgain(@Body() data: SendCodeAgainDto) {
+    return await this.authProvider.sendCodeAgain(data);
+  }
 
   @Post('/registrationFirstStep')
   public async registrationFirstStep(@Body() data: RegistrationFirstStepDto) {
